3b2b0be1ffc7e574482520285c6b9999b86ff2c3
Rogier <rogier@brandshakes.com>
2024-08-06 14:52:29 +0200

    Added bottom menu for mobile

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import Navbar from "./NavBar";
 import SessionProvider from "./SessionProvider";
+import MenuBar from "./MenuBar";
 
 export default async function Layout({
   children,
@@ -16,7 +17,11 @@ export default async function Layout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
+
       </div>
     </SessionProvider>
   );
