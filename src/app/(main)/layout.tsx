0c592697dfd6363255fadd3a80499de6104ab44e
Rogier <rogier@brandshakes.com>
2024-08-07 13:37:33 +0200

    Added responsive side-menu

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -17,11 +17,11 @@ export default async function Layout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+        <div className="mx-auto flex w-full max-w-full grow gap-5 p-1">
+        <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-2 rounded-2xl bg-card px-1 py-2 shadow-sm sm:block lg:px-1 xl:w-40" />
           {children}
         </div>
         <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
-
       </div>
     </SessionProvider>
   );
